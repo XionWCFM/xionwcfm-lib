d4329206ebca910dca17e669c1f48a0491aed4d6
devgiljong <calf-love@naver.com>
2024-09-04 03:40:35 +0900

    :construction: Version release

--- packages/react-query/src/index.ts
+++ packages/react-query/src/index.ts
@@ -1,7 +1,15 @@
 import { createMutationOptions } from "./create-mutation-options";
 import { createQueryOptions } from "./create-query-options";
+import { MutationBoundary } from "./mutation-boundary";
 import { mutationOptions } from "./mutation-options";
 import { useOptimisticMutation } from "./use-optimistic-mutation";
 import { usePollingQuery } from "./use-polling-query";
 
-export { mutationOptions, useOptimisticMutation, usePollingQuery, createMutationOptions, createQueryOptions };
+export {
+  mutationOptions,
+  useOptimisticMutation,
+  usePollingQuery,
+  createMutationOptions,
+  createQueryOptions,
+  MutationBoundary,
+};
