b7553409516843f2da05d48efe016620f842f5f9
devgiljong <calf-love@naver.com>
2024-09-04 03:48:24 +0900

    :sparkles: Version release

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -6,4 +6,6 @@ export default defineConfig({
   sourcemap: true,
   dts: true,
   clean: true,
+  minify: true,
+  treeshake: true,
 });
--- packages/xds/tsup.config.ts
+++ packages/xds/tsup.config.ts
@@ -7,4 +7,5 @@ export default defineConfig({
   dts: true,
   clean: true,
   minify: true,
+  treeshake: true,
 });
