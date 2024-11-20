47ab58572bfbb78171be6c9cf748e1817379e23c
devgiljong <calf-love@naver.com>
2024-11-20 23:37:28 +0900

    chore: packages build

--- packages/jotai/src/index.ts
+++ packages/jotai/src/index.ts
@@ -1,4 +1,4 @@
+import { createUseAtom, createUseAtomValue, createUseSetAtom } from "./create-atom-hook";
 import { createReusableAtom } from "./create-reusable-atom";
 import { createSafeAtom } from "./create-safe-atom";
-
-export { createSafeAtom, createReusableAtom };
+export { createSafeAtom, createReusableAtom, createUseSetAtom, createUseAtom, createUseAtomValue };
