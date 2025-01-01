b9a245377ae02ebc200fbd44f7a89dc68383a30c
YuGilJong <101111364+XionWCFM@users.noreply.github.com>
2025-01-01 17:24:29 +0900

    feat: cn develop (#29)

--- packages/xds/src/cn.ts
+++ packages/xds/src/cn.ts
@@ -1,11 +1,19 @@
 import { type ClassValue, clsx } from "clsx";
 import { extendTailwindMerge } from "tailwind-merge";
 
-import { XION_STYLE } from "@xionwcfm/token";
+import { vars } from "@xionwcfm/token";
 
 const customTwMerge = extendTailwindMerge({
-  classGroups: {
-    "font-size": [{ text: Object.keys(XION_STYLE.fontSize) }],
+  extend: {
+    theme: {
+      colors: Object.entries(vars.colors).map(([key, value]) => {
+        const childKeys = typeof value === "object" ? Object.keys(value) : [value];
+        return { [key]: childKeys };
+      }),
+    },
+    classGroups: {
+      "font-size": Object.keys(vars.fontSize).map((item) => `text-${item}`),
+    },
   },
 });
 
